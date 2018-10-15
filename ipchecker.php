6abb39a6c83e6fc23504d29ec053c9a038d81cd3
ananddanana <43434541+ananddanana@users.noreply.github.com>
2018-10-15 08:03:30 -0400

    Add files via upload

---
+++ ipchecker.php
@@ -0,0 +1,7 @@
+<h1>Am i Reachable??</h1>
+<form name="ping" action="posting.php" method="post">
+			<p>
+				Enter the IP address or domain :
+				<input type="text" name="ip" size=\"30\">
+				<input type="submit" name="Submit" value="Submit">
+			</p></form>
